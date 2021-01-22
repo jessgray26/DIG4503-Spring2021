a5fe42d8fadc0998051f627f479f70026352c3c7
Jessica Gray <37873440+jessgray26@users.noreply.github.com>
2021-01-21 21:59:56 -0500

    Lab 1: ES6 Classes

---
+++ lab1/Jessica.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+import Person from './Person.js';
+
+class Jessica extends Person{
+    constructor(){
+        super("Jessica","#6D27F1")
+    }
+}
+
+export default Jessica;
---
+++ lab1/index.js
@@ -0,0 +1,11 @@
+import Person from './Person.js';
+
+
+const p = new Person("Dan", "#F9E416");
+p.speak();
+
+
+import Jessica from './Jessica.js';
+
+const p2 = new Jessica();
+p2.speak();
