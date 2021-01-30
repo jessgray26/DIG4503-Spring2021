9ea86cdf6de6ed8708eb8dcd4eddc801d1726bd9
Jessica Gray <37873440+jessgray26@users.noreply.github.com>
2021-01-30 13:44:38 -0500

    Add files via upload

--- lab 2/index.js
+++ lab 2/index.js
@@ -5,5 +5,5 @@ const p = new Fetch("suicune", "#006DFF");
 
 const e = new Fetch("suicuneeeeeeeeee", "#006DFF");
     e.fetch();
-
+// I felt like I am on the right track but I could not find a way to call the fetch() functions separately. I think I missed something with setting function parameters.
 export {p,e};
