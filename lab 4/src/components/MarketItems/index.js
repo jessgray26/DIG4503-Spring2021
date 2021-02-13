0cd315815a8b07d944b9f7ec53f9c59536169536
Jessica Gray <37873440+jessgray26@users.noreply.github.com>
2021-02-12 20:52:51 -0500

    Lab 4: Components and State

---
+++ lab 4/src/components/MarketItems/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MarketItems = (props) => (
+        <div> 
+            <p>Item: {props.count}</p>
+            
+        </div>
+    );
+
+export default MarketItems;
