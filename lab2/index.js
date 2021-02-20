cc73cb233d3f8962d37522c16752a2420889a9d3
Jessica Gray <37873440+jessgray26@users.noreply.github.com>
2021-02-20 01:42:27 -0500

    Delete

--- lab2/index.js
+++
@@ -1,9 +0,0 @@
-import Fetch from './Fetch.js';
-
-const p = new Fetch("suicune", "#006DFF");
-    p.fetch();
-
-const e = new Fetch("suicuneeeeeeeeee", "#006DFF");
-    e.fetch();
-// I felt like I am on the right track but I could not find a way to call the fetch() functions separately. I think I missed something with setting function parameters.
-export {p,e};
