3af619cdff8e8d5cc13df4ebca88bfba334dca36
Jessica Gray <37873440+jessgray26@users.noreply.github.com>
2021-02-20 01:46:39 -0500

    Lab 3: React Rendering

---
+++ lab3/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from './components/Homepage';
+
+class App extends React.Component {
+  render(){
+    return(
+      <Homepage firstName="Jessica"/>
+    );
+  }
+}
+
+export default App;
